6465df1c214710216e3ce412e4076c99ddcf2a71
Ayesha Khan <fizaayesha696@gmail.com>
2025-04-16 11:09:50 +0530

    Initial COmmit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import AutoForm from '@/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <AutoForm type="sign-in"/>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AutoForm from '@/components/AuthForm';
+
+const page = () => {
+  return (
+    <AutoForm type="sign-up"/>
+  )
+}
+
+export default page
